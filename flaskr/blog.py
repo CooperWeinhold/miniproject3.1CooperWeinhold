c3a0b06d60a886b6cb65eaa17b330b60e5f9926e
Cooper Weinhold <cjweinhold@mail.fhsu.edu>
2024-10-23 20:51:38 -0500

    Changed the theme around and fixed buttons covering description

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -44,3 +44,55 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    db = get_db()
+    aircrafts = db.execute(
+        'SELECT * FROM aircrafts WHERE id = ? AND author_id = ?',
+        (id, g.user['id'])
+    ).fetchone()
+
+    if aircrafts is None:
+        abort(404, "Aircraft id {0} doesn't exist or you don't have permission.".format(id))
+
+    if request.method == 'POST':
+        name = request.form['name']
+        aircraft_type = request.form['aircraft_type']
+        manufacturer = request.form['manufacturer']
+        description = request.form['description']
+        error = None
+
+        if not name:
+            error = 'Aircraft name is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db.execute(
+                'UPDATE aircrafts SET name = ?, aircraft_type = ?, manufacturer = ?, description = ? WHERE id = ?',
+                (name, aircraft_type, manufacturer, description, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', aircraft=aircrafts)
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    db = get_db()
+    aircrafts = db.execute(
+        'SELECT id FROM aircrafts WHERE id = ? AND author_id = ?',
+        (id, g.user['id'])
+    ).fetchone()
+
+    if aircrafts is None:
+        abort(404, "Aircraft id {0} doesn't exist or you don't have permission.".format(id))
+
+    db.execute('DELETE FROM aircrafts WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
